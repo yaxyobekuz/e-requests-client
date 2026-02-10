0b65558c5a4059e5d5ef007980829f446693891d
yaxyobekuz <pubgn9642@gmail.com>
2026-02-10 16:36:14 +0500

    integrate query provider and devtools

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // Styles
 import "./styles/index.css";
 
-// Roaster
+// Toaster
 import { Toaster } from "sonner";
 
 // Routes
@@ -11,6 +11,11 @@ import Routes from "@/app/routes.jsx";
 import store from "@/app/store";
 import { Provider } from "react-redux";
 
+// TanStack Query
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "@/app/query-client";
+
 // React
 import { createRoot } from "react-dom/client";
 
@@ -19,15 +24,19 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <Routes />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Routes />
 
-      <Toaster
-        richColors
-        position="top-right"
-        offset={{ top: 24 }}
-        mobileOffset={{ top: 24 }}
-      />
-    </Provider>
+        <Toaster
+          richColors
+          position="top-right"
+          offset={{ top: 24 }}
+          mobileOffset={{ top: 24 }}
+        />
+      </Provider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
