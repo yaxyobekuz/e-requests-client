d3f83aaf5aac397162026a19447c57a661228ea4
yaxyobekuz <pubgn9642@gmail.com>
2026-02-14 13:06:09 +0500

    feat: add navigation and stories data for dashboard components

---
+++ src/features/dashboard/data/stories.data.js
@@ -0,0 +1,31 @@
+const stories = [
+  {
+    id: 23452,
+    name: "Murojaat",
+    description: "Ariza va shikoyatlarni onlayn yuborish imkoniyati",
+    image: "https://i.pinimg.com/736x/99/a1/db/99a1db6c2df3d4662c930a3ab6af4ceb.jpg",
+    previewImage: "https://img.freepik.com/free-psd/3d-rendered-purple-folder-icon-dark-purple-background_84443-56534.jpg?semt=ais_wordcount_boost&w=740&q=80",
+    button: "Boshlash",
+    link: "/requests/new",
+  },
+  {
+    id: 12123,
+    name: "Xizmatlar",
+    description: "Elektr, gaz va suv ta'minoti xizmatlaridan foydalanish",
+    image: "https://i.pinimg.com/736x/79/6c/8e/796c8eca56d85786a9303df85ec49697.jpg",
+    previewImage: "https://i.pinimg.com/236x/bf/03/03/bf03036aa661d5ffd9cb4992960af3b9.jpg",
+    button: "Ko'rish",
+    link: "/services",
+  },
+  {
+    id: 331231232,
+    name: "MSK",
+    description: "Mahallangizdagi servis kompaniyasi faoliyati",
+    image: "https://thumbs.dreamstime.com/b/handyman-repairing-central-heating-boiler-room-40470599.jpg",
+    previewImage: "https://t3.ftcdn.net/jpg/16/71/71/64/360_F_1671716471_Ojv6iomzQUSfn6VQSmImfAf0WeHMJDdA.jpg",
+    button: "Batafsil",
+    link: "/msk",
+  },
+];
+
+export default stories;
