2689743330dcf23de64cda57a4e29f1000394ff6
yaxyobekuz <pubgn9642@gmail.com>
2026-02-15 20:27:21 +0500

    feat: update Card component styles for improved layout and spacing

--- src/shared/components/ui/Card.jsx
+++ src/shared/components/ui/Card.jsx
@@ -17,11 +17,11 @@ const Card = ({ className = "", children, responsive = false, title = "" }) => {
       className={cn(
         responsive
           ? "xs:p-5 xs:rounded-2xl xs:bg-white"
-          : "bg-white p-5 rounded-2xl",
+          : "bg-white p-3.5 rounded-2xl xs:p-5",
         className,
       )}
     >
-      {title && <h2 className="font-semibold mb-3.5 text-blue-600">{title}</h2>}
+      {title && <h2 className="font-semibold mb-2 text-blue-600 xs:mb-3.5">{title}</h2>}
       {children}
     </div>
   );
