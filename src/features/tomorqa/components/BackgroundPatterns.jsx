cbd4f4c49bc332a193fc1bd5547f1f67b6648290
yaxyobekuz <pubgn9642@gmail.com>
2026-03-13 22:46:22 +0500

    feat: add BackgroundPatterns component for decorative background elements

---
+++ src/features/tomorqa/components/BackgroundPatterns.jsx
@@ -0,0 +1,10 @@
+const BackgroundPatterns = () => (
+  <>
+    <div className="fixed inset-0 z-0 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:24px_24px]" />
+    <div className="fixed left-0 right-0 top-[-10%] z-0 m-auto h-[300px] w-[300px] rounded-full bg-blue-500 opacity-20 blur-[100px]" />
+    <div className="fixed bottom-[-10%] left-[-10%] z-0 h-[300px] w-[300px] rounded-full bg-indigo-500 opacity-20 blur-[100px]" />
+    <div className="fixed top-[20%] right-[-5%] z-0 h-[250px] w-[250px] rounded-full bg-purple-500 opacity-20 blur-[100px]" />
+  </>
+);
+
+export default BackgroundPatterns;
