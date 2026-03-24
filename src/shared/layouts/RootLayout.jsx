97759513cc40c9c1e00c71711485a61c701fe4e8
yaxyobekuz <pubgn9642@gmail.com>
2026-03-24 23:10:31 +0500

    feat: add BugReport component for user feedback submission

--- src/shared/layouts/RootLayout.jsx
+++ src/shared/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 // Router
 import { Outlet } from "react-router-dom";
 
+// Components
+import BugReport from "../components/layout/BugReport";
+
 const RootLayout = () => {
   return (
     <div className="bg-gray-100">
       <Outlet />
+      <BugReport />
     </div>
   );
 };
